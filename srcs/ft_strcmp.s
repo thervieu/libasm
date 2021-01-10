c7c458b9f8305ae0fd5a0ad81869a3d676ee4328
thervieu <thibault99hervieu@gmail.com>
2021-01-10 18:53:37 +0100

    removed unnecessary files, changed strcmp so that it returns the right value

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -6,21 +6,20 @@ ft_strcmp:	; rsi = str1  rdi = str2  rax = return_value
 	xor 	rdx, rdx	; rdx = 0
 
 cmp_loop:
-	mov		cl, byte [rsi + rdx]	; cl = char *tmp used because we cant compare
-	cmp		byte [rdi + rdx], cl	; two byte []
-	jl		lower					; jl = jump if lower
-	jg		greater					; jg = jump if greater
-	cmp		cl, 0					; if rsi[rdx] == 0
+	mov 	cl, byte [rsi + rdx]
+	mov		al, byte [rdi + rdx]	; cl = char *tmp used because we cant compare
+	cmp		cl, al					; two byte []
+	jl		diff					; jl = jump if lower
+	jg		diff					; jg = jump if greater
+	cmp		al, 0					; if rsi[rdx] == 0
 	jz		end						; jz = jump if null
 	inc		rdx						; inc = increment
 	jmp		cmp_loop				; while loop
 
-lower:
-	mov		rax, -1					; rax = -1
-	jmp end	
-	
-greater:
-	mov		rax, 1					; rax = 1
+diff:
+	movzx rcx, cl					; movzx is just mov from smaller registeries
+	movzx rax, al					; to bigger and add zeroes where you must
+	sub rax, rcx					; get diff
 
 end:
-	ret
+	ret								; return diff
